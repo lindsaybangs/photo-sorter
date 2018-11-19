abb41d95d0bbf33a8151b889d17287ffc8373f5f
Lindsay Bangs <lindsaybangs@users.noreply.github.com>
2018-11-18 19:26:39 -0500

    Removed commented-out lines from unpacking tool

--- unpack.py
+++ unpack.py
@@ -4,33 +4,23 @@ from os.path import join, isdir
 from os import listdir, rmdir, rename
 
 def unpack(path_to_file):
-	for directory in listdir(path_to_file):
-		#print directory
-		
+	for directory in listdir(path_to_file):		
 		parent_path = join(path_to_file, directory)
-		#print parent_path
 
 		if not isdir(parent_path): 
 			continue
 
 		for file in listdir(parent_path):
-#			print file
 			curr_path = join(parent_path, file)
 			new_path = join(path_to_file, file)
-#			print "{0} --> {1}".format(curr_path, new_path)
 
 			if isdir(curr_path): 
 				continue
 			rename(curr_path, new_path)
 		rmdir(parent_path)
-#		print ".."
-
-
 
 def main():
 	path_to_file = sys.argv[1]
-#	print "Directory: ", path_to_file
 	unpack(path_to_file)
-
 
 if __name__ == "__main__": main()
